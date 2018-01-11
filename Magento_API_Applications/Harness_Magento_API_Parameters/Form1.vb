8122aee05c9b6b925523302f031dc7d2ea724bbf
Jerome Boyer <jeromechboyer@gmail.com>
2018-01-11 09:52:46 -0800

    Includes Status For: https://www.mapleleafpromostore.com/index.php/api/v2_soap/index/
    https://www.mapleleafpromostore.com/index.php/api/v2_soap/index/

--- Magento_API_Applications/Harness_Magento_API_Parameters/Form1.vb
+++ Magento_API_Applications/Harness_Magento_API_Parameters/Form1.vb
@@ -1,4 +1,5 @@
-﻿'RELEASED and PUBLISHED Aug 18, 2017
+﻿'RELEASED and PUBLISHED Aug 18, 2017, Jan 11 2018
+'for:   'https://www.mapleleafpromostore.com/index.php/api/v2_soap/index/
 Public Class Form1
     Private Sub bGetSessionID_Click(sender As Object, e As EventArgs) Handles bGetSessionID.Click
 
