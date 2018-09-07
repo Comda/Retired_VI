3f02060c8d08bacaeb7b2daf43d2c675e961fc87
Jerome Boyer <jeromechboyer@gmail.com>
2018-09-06 23:06:54 -0700

    New Branch for Product_QA

---
+++ PROD/Comda_com_Catalog_CLR/Comda_com_Catalog_CLR/MagentoProducts.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Data.SqlTypes
+Imports COMDA_COM_Product_QA
+Imports Microsoft.SqlServer.Server
+
+
+Partial Public Class StoredProcedures
+    <Microsoft.SqlServer.Server.SqlProcedure()>
+    Public Shared Sub MagentoProducts_GET(ByVal UserId As String, ByVal API_ID As String, ByVal ConnectionString As String, ByVal ImportID As Guid)
+
+        Dim SessionId As String = Nothing
+        Dim MagentoLogin As New Magento_Login(UserId, API_ID, SessionId)
+        Dim dbConnection As New SqlConnection With {
+            .ConnectionString = ConnectionString
+        }
+
+        Dim BasicCatalog As New BaseProducts(SessionId, dbConnection, ImportID)
+        Dim DetailedCatalog As New DetailedProducts(SessionId, dbConnection, ImportID)
+
+    End Sub
+End Class
---
+++ PROD/Comda_com_Catalog_CLR/Comda_com_Catalog_CLR/MagentoSessionID.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Data.SqlTypes
+Imports COMDA_COM_Product_QA
+Imports Microsoft.SqlServer.Server
+
+Partial Public Class UserDefinedFunctions
+    <Microsoft.SqlServer.Server.SqlFunction()>
+    Public Shared Function MagentoSessionID_GET(ByVal UserId As String, ByVal API_ID As String) As SqlString
+        Dim SessionId As String = Nothing
+        Dim MagentoLogin As New Magento_Login(UserId, API_ID, SessionId)
+        Return SessionId
+    End Function
+End Class
